ce00bc630961050cf0db55df458261ae34568763
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2024-02-23 18:07:53 +0100

    test: create a new match with score 0-0

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,15 +1,26 @@
 import { describe, it } from "node:test";
 import assert from "node:assert/strict";
+import { Tournament } from "./index";
 
 void describe(`start a new match`, () => {
-  it(`should start a new match`, async () => {
+  it(`should start a new match with score 0 for the local team and 0 for the visitor team`, async () => {
     // create tournament
+    const worldCup = new Tournament("Fifa World Cup 2024");
 
     // add teams to the tournament
+    const crc = worldCup.addTeam({ id: "CRC" });
+    const nor = worldCup.addTeam({ id: "NOR" });
 
     // create a match
+    worldCup.addMatch(crc, nor);
 
-    // check id of the match 
-    assert.equal(1, 1);
+    const match = worldCup.getMatch(`${crc.id}-${nor.id}`);
+
+    // check id of the match
+    assert.equal(match?.id, "CRC-NOR");
+
+    // check score
+    assert.equal(match.localScore, 0);
+    assert.equal(match.visitorScore, 0);
   });
 });
---
+++ src/index.ts
@@ -0,0 +1,50 @@
+type Team = { id: string };
+
+type MatchStatus = "active" | "finish";
+type Match = {
+  id: string;
+  local: Team;
+  localScore: number;
+  visitor: Team;
+  visitorScore: number;
+  startTime: Date;
+  status: MatchStatus;
+};
+
+export class Tournament {
+  private name: string;
+  private teams: Team[] = [];
+  private matches: Match[] = [];
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  public addTeam = (team: Team): Team => {
+    this.teams.push(team);
+    return team;
+  };
+
+  public addMatch = (localTeam: Team, visitorTeam: Team) => {
+    const match = {
+      id: `${localTeam.id}-${visitorTeam.id}`,
+      local: localTeam,
+      localScore: 0,
+      visitor: visitorTeam,
+      visitorScore: 0,
+      startTime: new Date(),
+      status: "active" as MatchStatus,
+    };
+    this.matches.push(match);
+  };
+
+  public getMatch = (matchId: string) => {
+    const match = this.matches.filter((match) => match.id === matchId);
+    return match[0];
+  };
+
+  public getLiveScoreboard = () =>
+    this.matches.filter((match) => match.status === "active");
+
+  public getName = () => this.name;
+}
