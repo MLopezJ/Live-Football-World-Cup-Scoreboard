a3472eb8a07ed1cf2e9bdbb6c82e1e967ca85d79
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2024-02-23 17:46:11 +0100

    test: draft steps to start a new match

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,9 +1,15 @@
-import { describe, it } from 'node:test'
-import assert from 'node:assert/strict'
+import { describe, it } from "node:test";
+import assert from "node:assert/strict";
 
 void describe(`start a new match`, () => {
-	it(`should start a new match`, async () => {
-        // Dummy test
-		assert.equal(1, 1)
-	})
-})
+  it(`should start a new match`, async () => {
+    // create tournament
+
+    // add teams to the tournament
+
+    // create a match
+
+    // check id of the match 
+    assert.equal(1, 1);
+  });
+});
