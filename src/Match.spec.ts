569fbee989c2bf51b76bb428253085ce458eff10
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2024-02-24 15:38:55 +0100

    test: check score increase when goal is whole number

--- src/Match.spec.ts
+++ src/Match.spec.ts
@@ -10,27 +10,37 @@ void describe(`Match`, () => {
     assert.throws(() => new Match(local, visitor), Error);
   });
 
-  for (const [token] of [
-    [-1],
-    ["hei"],
-    [3.14],
-    [-0],
-    ["??!!%%$$##"],
-    [-2.6788],
-  ] as [string][]) {
-    /**
-     * Whole numbers are a set of numbers including all natural numbers and 0. 
-     * They are a part of real numbers that do not include fractions, decimals, or negative numbers. 
-     * @see https://www.cuemath.com/numbers/whole-numbers/
-     */
-    void it(`goals must be whole numbers`, () => {
+  void describe(`Increase score`, () => {
+    for (const [token] of [
+      [-1],
+      ["hei"],
+      [3.14],
+
+      ["??!!%%$$##"],
+      [-2.6788],
+      ["2"],
+    ] as [string][]) {
+      void it(`should not increase the score if any of the goals is not whole numbers: token ${token}`, () => {
         const local = { id: "CRC" };
         const visitor = { id: "NOR" };
-        
-        const match = new Match(local, visitor)
 
-        match.setGoal((token as unknown as number), (token as unknown as number))
-        assert.equal(match.getTotalAmountOfGoals(),0)
-    });
-  }
+        const match = new Match(local, visitor);
+
+        match.setGoal(token as unknown as number, 7);
+        assert.equal(match.getTotalAmountOfGoals(), 0);
+      });
+    }
+
+    for (const [goal] of [[1], [100], [3], [10], [0]] as [number][]) {
+      void it(`should increase the score when the goals are whole numbers: goal ${goal}`, () => {
+        const local = { id: "CRC" };
+        const visitor = { id: "NOR" };
+
+        const match = new Match(local, visitor);
+
+        match.setGoal(goal, 0);
+        assert.equal(match.getTotalAmountOfGoals(), goal);
+      });
+    }
+  });
 });
